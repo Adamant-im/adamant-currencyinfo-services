c3c1e9adbf130d93283bf4f578492992f008625b
RomanSlep <slep48@gmail.com>
2019-02-18 14:39:27 +0300

    fixed hardcore data

--- app.js
+++ app.js
@@ -2,31 +2,33 @@ const currencys = {};
 const Moex = require('./helpers/getMoex');
 const Cmc = require('./helpers/getCmc');
 const router = require('./modules/router');
+const {refreshInterval, baseCoins}=require('./config.json');
 const db = require('./db/mongodb');
+
 router(currencys);
 
 function refresh() {
     console.log('------------ Update -------------');
     Moex(data => {
         if (data) Object.assign(currencys, data)
-    });
-
-    ['USD', 'RUB', 'EUR', 'CNY', 'JPY', 'BTC', 'ETH'].forEach(b => {
+	});
+	
+    baseCoins.forEach(b => {
         Cmc(b, data => {
             if (data) Object.assign(currencys, data);
-        });
-    });
-
+		});
+	});
+	
     setTimeout(() => {
         db.save(currencys, (res) => {
             if (res)
-                console.log('Success saved currencys');
+			console.log('Success saved currencys');
             else
-                console.log('Error saved currencys');
-        });
-    }, 10000);
+			console.log('Error saved currencys');
+		});
+	}, 10000);
 }
 
 refresh();
 
-setInterval(refresh, 600 * 1000);
+setInterval(refresh, refreshInterval*60000);
